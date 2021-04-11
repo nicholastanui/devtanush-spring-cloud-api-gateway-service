5cc8247c64e557d56190c8d32f8cf3d27c7eab53
ntanui <ntanui@safaricom.co.ke>
2021-04-11 21:54:11 +0300

    init setup

---
+++ src/main/java/com/devtanush/microservices/springcloudapigatewayservice/devtanushspringcloudapigatewayservice/DevtanushSpringCloudApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.springcloudapigatewayservice.devtanushspringcloudapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtanushSpringCloudApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtanushSpringCloudApiGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devtanush/microservices/springcloudapigatewayservice/devtanushspringcloudapigatewayservice/DevtanushSpringCloudApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.springcloudapigatewayservice.devtanushspringcloudapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtanushSpringCloudApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
